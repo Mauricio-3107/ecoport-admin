3342e422051ca6112ea00d803b181b679827eac1
Mauri <ramirezcalustrom@gmail.com>
2025-04-16 00:38:59 -0400

    Deleted files and dirs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
-import Users from "./pages/Users";
-import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
@@ -17,8 +13,6 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Clients from "./pages/Clients";
 import Company from "./pages/Company";
-import Booking from "./pages/Booking";
-import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import Pickup from "./pages/Pickup";
@@ -60,16 +54,11 @@ function App() {
             >
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<Checkin />} />
               <Route path="trips" element={<Trips />} />
-              <Route path="cabins" element={<Cabins />} />
               <Route path="trucks" element={<Trucks />} />
               <Route path="drivers" element={<Drivers />} />
               <Route path="clients" element={<Clients />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
+              {/* <Route path="users" element={<Users />} /> */}
               <Route path="company" element={<Company />} />
               <Route path="account" element={<Account />} />
               <Route path="pickup" element={<Pickup />} />
