c8b9c850989142f887dad34f56ab0e0aa98fd303
Mauri <ramirezcalustrom@gmail.com>
2025-04-16 20:56:51 -0400

    Add Netlify redirects for SPA routing

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("ecoportsrl@hotmail.com");
-  const [password, setPassword] = useState("Volvo20trucks");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { isLoading, login } = useLogin();
 
   function handleSubmit(e) {
