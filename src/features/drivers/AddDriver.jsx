c5f2b384a46d6fb4af31bcbb4e72d64c82a5a6d3
Mauri <ramirezcalustrom@gmail.com>
2025-04-26 16:00:56 -0400

    Drivers mobile

--- src/features/drivers/AddDriver.jsx
+++ src/features/drivers/AddDriver.jsx
@@ -1,8 +1,11 @@
+import useMediaQuery from "../../hooks/useMediaQuery";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateDriverForm from "./CreateDriverForm";
 
 function AddDriver() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  if (isMobile) return null;
   return (
     <div>
       <Modal>
--- src/features/trucks/AddTruck.jsx
+++ src/features/trucks/AddTruck.jsx
@@ -1,8 +1,11 @@
+import useMediaQuery from "../../hooks/useMediaQuery";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateTruckForm from "./CreateTruckForm";
 
 function AddTruck() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  if (isMobile) return null;
   return (
     <div>
       <Modal>
